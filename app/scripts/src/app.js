77677506777e37a20f29693ca9ecf4a74ad5f23e
smaran39 <smaran3939@gmail.com>
2017-04-24 14:58:42 -0700

    finished chapter 18 and corrected all errors

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,24 +1,56 @@
-import socket from './ws-client'
+import socket from './ws-client';
+import {
+    UserStore
+} from './storage';
+import {
+    ChatForm,
+    ChatList,
+    promptForUsername
+} from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
+const LIST_SELECTOR = '[data-chat="message-list"]';
+
+let userStore = new UserStore('x-chattrbox/u'); /* 'x-chattrbox/u' is the 'key' for the sessionStorage here. It is given as one fixed because only one user will use the browser tab. If it's a real-time app then key must be unique! */
+let username = userStore.get();
+if (!username) {
+    username = promptForUsername();
+    userStore.set(username);
+}
 
 class ChatApp {
     constructor() {
-        //console.log('Hello ES6!');
+        this.chatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+        this.chatList = new ChatList(LIST_SELECTOR, username);
+
         socket.init('ws://localhost:3001');
-        socket.registerOpenHandler(() => {
+        /*socket.registerOpenHandler(() => {
             let message = new ChatMessage({
                 message: 'pow!'
             });
             socket.sendMessage(message.serialize());
+        });*/
+        socket.registerOpenHandler(() => {
+            this.chatForm.init((text) => {
+                let message = new ChatMessage({
+                    message: text
+                });
+                socket.sendMessage(message.serialize());
+            });
+            this.chatList.init();
         });
         socket.registerMessageHandler((data) => {
             console.log(data);
+            let message = new ChatMessage(data);
+            this.chatList.drawMessage(message.serialize());
         });
     }
 }
 class ChatMessage {
     constructor({
         message: m,
-        user: u = 'batman',
+        user: u = username,
         timestamp: t = (new Date()).getTime()
     }) {
         this.message = m;
--- app/scripts/src/ws-client.js
+++ app/scripts/src/ws-client.js
@@ -1,30 +1,32 @@
 let socket;
 
 function init(url) {
-  socket = new WebSocket(url);
-  console.log('Connecting...');
+    socket = new WebSocket(url);
+    console.log('Connecting...');
 }
 
 function registerOpenHandler(handlerFunction) {
-  socket.onopen=() =>{
-    console.log('open');
-    handlerFunction();
-  };
-}
-function registerMessageHandler(handlerFunction) {
-  socket.onmessage = (e) => {
-    console.log('message',e.data);
-    let data = JSON.parse(e.data);
-    handlerFunction(data);
-  };
-}
-function sendMessage(payload) {
-  socket.send(JSON.stringify(payload));
+    socket.onopen = () => {
+        console.log('open');
+        handlerFunction();
+    };
 }
 
-export default{
-  init,
-  registerOpenHandler,
-  registerMessageHandler,
-  sendMessage
+function registerMessageHandler(handlerFunction) {
+    socket.onmessage = (e) => {
+        console.log('message', e.data);
+        let data = JSON.parse(e.data);
+        handlerFunction(data);
+    };
 }
+
+function sendMessage(payload) {
+    socket.send(JSON.stringify(payload));
+}
+
+export default {
+    init,
+    registerOpenHandler,
+    registerMessageHandler,
+    sendMessage
+}; //added semicolon for eslinter purpose
